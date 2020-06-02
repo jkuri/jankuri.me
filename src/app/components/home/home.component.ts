25ba8abe84dc64752bf9ae0d556ebcb7ac0f7669
Jan Kuri <jkuri88@gmail.com>
2020-06-02 12:11:55 +0200

    chore(): comets

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ElementRef, HostListener, PLATFORM_ID, Inject } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener, PLATFORM_ID, Inject, OnDestroy } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-import { select } from 'd3-selection';
+import { interval, Subscription } from 'rxjs';
+import { select, Selection, BaseType } from 'd3-selection';
 import { scaleLinear } from 'd3-scale';
 import { easeLinear } from 'd3-ease';
 import 'd3-transition';
@@ -9,17 +10,21 @@ import 'd3-transition';
   selector: 'app-home',
   templateUrl: 'home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   private isBrowser: boolean = isPlatformBrowser(this.platform_id);
   bgElement: HTMLElement;
   w: number;
   h: number;
   g: any;
   x: number;
+  n = 100;
+  comet: Selection<BaseType, unknown, HTMLElement, any>;
+  sub: Subscription = new Subscription();
+  intervalMs = 5000;
 
   constructor(private elementRef: ElementRef, @Inject(PLATFORM_ID) private platform_id) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     if (!this.isBrowser) {
       return;
     }
@@ -28,26 +33,41 @@ export class HomeComponent implements OnInit {
     this.render();
     this.x = 0;
     this.animation();
+    this.sub.add(this.cometTwinkle());
   }
 
-  animation = () => {
-    this.x = (this.x < 30) ? this.x += 1 : 0;
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  private cometTwinkle(): Subscription {
+    return interval(this.intervalMs)
+      .subscribe(() => {
+        this.comet
+          .style('left', `${this.getRandomInt(10, 90)}%`)
+          .style('top', `${this.getRandomInt(10, this.h / 2)}px`)
+          .style('animation', `cometShorter ${this.intervalMs}ms cubic-bezier(0,.53,.35,.99) 0s infinite`);
+      });
+  }
+
+  private animation = () => {
+    this.x = (this.x < this.n - 1) ? this.x += 1 : 0;
     this.g.selectAll('circle')
       .transition()
-      .duration(650)
+      .duration(this.intervalMs)
       .ease(easeLinear)
-      .attr('cx', (d: any, i: number) => {
-        const move = ((this.w / 30) * i) + (this.w / 30) * this.x;
+      .attr('cx', (_: any, i: number) => {
+        const move = ((this.w / this.n) * i) + (this.w / this.n) * this.x;
         return (move > this.w) ? move - this.w : move;
       })
-      .on('end', (val: number, size: number) => {
-        if (size === 29) {
+      .on('end', (_: number, size: number) => {
+        if (size === this.n - 1) {
           this.animation();
         }
       });
   }
 
-  render() {
+  private render(): void {
     this.w = this.bgElement.clientWidth;
     this.h = this.bgElement.clientHeight;
     select(this.bgElement).select('svg').remove();
@@ -55,45 +75,15 @@ export class HomeComponent implements OnInit {
     const svg = select(this.bgElement).append('svg').attr('width', this.w).attr('height', this.h);
     let g = svg.append('g');
 
-    let data = [20, 24, 20, 20, 25, 27, 26, 20, 21, 22, 24, 20, 28, 20, 28, 26, 25, 28, 22, 24, 29, 21, 30, 21, 27, 22, 23, 28, 30, 24];
+    let data = [...new Array(this.n)].map(() => this.getRandomInt(0, 10));
     const x = scaleLinear().domain([0, data.length - 1]).range([0, this.w]);
     const y = scaleLinear().domain([0, 150]).range([this.h, 0]);
 
     this.g = svg.append('g');
+    this.comet = select('.comet');
 
-    this.g.selectAll('circle')
-      .data(data)
-      .enter()
-      .append('circle')
-      .attr('r', (d: any, i: number) => this.getRandomInt(this.w < 500 ? 5 : 10, this.w < 500 ? 10 : 30))
-      .attr('cx', (d: any, i: number) => (this.w / data.length) * i)
-      .attr('cy', (d: any, i: number) => this.getRandomInt(0, this.w))
-      .attr('fill', '#34BEEB');
+    this.generateStars();
 
-    g.selectAll('circle')
-      .data(data)
-      .enter()
-      .append('circle')
-      .attr('r', (d: any, i: number) => 105)
-      .attr('cx', (d: any, i: number) => x(i))
-      .attr('cy', (d: any, i: number) => y(d))
-      .attr('fill', '#63CCF0')
-      .attr('stroke', 'none');
-
-    data = [12, 14, 12, 17, 12, 14, 19, 13, 16, 17, 17, 14, 10, 19, 18, 16, 10, 11, 10, 12, 17, 14, 14, 13, 14, 13, 12, 10, 19];
-    g = svg.append('g');
-
-    g.selectAll('circle')
-      .data(data)
-      .enter()
-      .append('circle')
-      .attr('r', (d: any, i: number) => 145)
-      .attr('cx', (d: any, i: number) => x(i))
-      .attr('cy', (d: any, i: number) => y(d))
-      .attr('fill', '#A4E0F5')
-      .attr('stroke', 'none');
-
-    data = [1, 2, 0, 2, 7, 1, 0, 6, 6, 6, 1, 4, 8, 10, 7, 8, 9, 4, 7, 2, 4, 2, 4, 9, 2, 7, 2, 10, 2, 10];
     g = svg.append('g');
 
     g.selectAll('circle')
@@ -103,7 +93,7 @@ export class HomeComponent implements OnInit {
       .attr('r', (d: any, i: number) => 185)
       .attr('cx', (d: any, i: number) => x(i))
       .attr('cy', (d: any, i: number) => y(d))
-      .attr('fill', '#FFFFFF')
+      .attr('fill', '#ffffff')
       .attr('stroke', 'none');
 
     g = svg.append('g');
@@ -115,7 +105,21 @@ export class HomeComponent implements OnInit {
       .attr('y', this.w < 500 ? 50 : this.h / 4);
   }
 
-  getRandomInt(min: number, max: number): number {
+  private generateStars(): void {
+    this.g.selectAll('circle').remove();
+    [...new Array(this.n)]
+      .forEach(() => {
+        this.g.append('circle')
+          .attr('class', 'star')
+          .attr('r', `0.${this.getRandomInt(60, 99)}`)
+          .attr('fill', '#fff')
+          .attr('cx', Math.random() * this.w)
+          .attr('cy', Math.random() * this.h)
+          .style('animation', `${this.getRandomInt(1000, 10000)}ms ease-in-out 0s infinite normal none twinkle`)
+      });
+  }
+
+  private getRandomInt(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
