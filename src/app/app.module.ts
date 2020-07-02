a361804c8cfe117861df573155262dc08c071390
Jan Kuri <jkuri88@gmail.com>
2020-07-02 23:05:16 +0200

    chore(resume): style improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ResumeComponent } from './components/resume/resume.component';
 import { TooltipDirective } from './directives/tooltip.directive';
+import { TruncatePipe } from './pipes/truncate.pipe';
 
 @NgModule({
   imports: [CommonModule, BrowserModule.withServerTransition({ appId: 'jan-app' }), AppRoutingModule],
-  declarations: [AppComponent, HomeComponent, ResumeComponent, TooltipDirective],
+  declarations: [AppComponent, HomeComponent, ResumeComponent, TooltipDirective, TruncatePipe],
   exports: [AppComponent],
   bootstrap: [AppComponent]
 })
